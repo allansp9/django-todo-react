3a825f466c301dcbe7ee6b3af6a2107cbd23e25c
allansp9 <allansp9@gmail.com>
2021-05-21 16:04:21 -0300

    add Todo Admin

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    '''Admin View for Todo'''
+
+    list_display = ('title', 'description', 'completed')
 
 # Register your models here.
+
+
+admin.site.register(Todo, TodoAdmin)
