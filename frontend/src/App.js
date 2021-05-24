97f0f1a7a014c6b262c1973b902b32c7a6c46d39
allansp9 <allansp9@gmail.com>
2021-05-24 15:48:08 -0300

    add axios and remove hardcoded todoitems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,7 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
 
-const todoItems = [
-  {
-    id: 1,
-    title: "Go to Market",
-    description: "Buy ingredients to prepare dinner",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "Study",
-    description: "Read Algebra and History textbook for the upcoming test",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "Sammy's books",
-    description: "Go to library to return Sammy's books",
-    completed: true,
-  },
-  {
-    id: 4,
-    title: "Article",
-    description: "Write article on how to use Django with React",
-    completed: false,
-  },
-];
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
@@ -34,7 +9,7 @@ class App extends Component {
 
     this.state = {
       viewCompleted: false,
-      todoList: todoItems,
+      todoList: [],
       modal: false,
       activeItem: {
         title: "",
