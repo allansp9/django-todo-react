636a3cf87f472b7d1748b9f24a416043a7b8554b
allansp9 <allansp9@gmail.com>
2021-05-24 15:57:13 -0300

    add refreshList and handleSubmit functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,17 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    axios
+      .get("/api/todos/")
+      .then((res) => this.setState({ todoList: res.data }))
+      .catch((err) => console.log(err));
+  };
+
   toggle = () => {
     this.setState({ modal: !this.state.modal });
   };
@@ -26,11 +37,17 @@ class App extends Component {
   handleSubmit = (item) => {
     this.toggle();
 
-    alert("save" + JSON.stringify(item));
+    if (item.id) {
+      axios
+        .put(`/api/todos/${item.id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    axios.post("/api/todos/", item).then((res) => this.refreshList());
   };
 
   handleDelete = (item) => {
-    alert("delete" + JSON.stringify(item));
+    axios.delete(`/api/todos/${item.id}/`).then((res) => this.refreshList());
   };
 
   createItem = () => {
